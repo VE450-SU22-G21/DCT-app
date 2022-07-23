d7117569d92c9fecec17354044c819227661076b
tc-imba <liuyh615@126.com>
2022-07-23 19:27:10 +0800

    fix: styles

--- App.jsx
+++ App.jsx
@@ -1,9 +1,9 @@
-import { StatusBar } from 'expo-status-bar';
+import {StatusBar} from 'expo-status-bar';
 import * as React from 'react';
 import 'react-native-get-random-values';
-import { StyleSheet, View, ScrollView } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {StyleSheet, View, ScrollView} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {
   Button,
   BottomNavigation,
@@ -14,6 +14,7 @@ import {
   Avatar,
   Divider,
   List,
+  MD3LightTheme as DefaultTheme,
 } from 'react-native-paper';
 
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
@@ -24,22 +25,35 @@ import SuccessScreen from './src/screens/Success';
 
 const Stack = createNativeStackNavigator();
 
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  version: 3,
+  colors: {
+    ...DefaultTheme.colors,
+    surface: '#f2edf6',
+    // primary: '#3498db',
+    secondary: '#EADDFF',
+    // tertiary: '#a1b2c3',
+  },
+};
+
 export default function App() {
   return (
     // <View style={styles.container}>
     //   <Text>Open up App.jsx to start working on your app!</Text>
     //   <StatusBar style="auto" />
     // </View>
-    <Provider settings={{
-      icon: (props) => <MaterialIcons {...props} />,
-    }}
+    <Provider
+      settings={{icon: (props) => <MaterialIcons {...props} />}}
+      theme={theme}
     >
       <NavigationContainer>
         <Stack.Navigator>
           {/* <Stack.Screen name="Submission" component={SubmissionScreen} /> */}
-          <Stack.Screen name="Welcome" component={WelcomeScreen} />
-          <Stack.Screen name="Home" component={HomeScreen} />
-          <Stack.Screen name="Success" component={SuccessScreen} />
+          <Stack.Screen name="Welcome" component={WelcomeScreen}/>
+          <Stack.Screen name="Home" component={HomeScreen}/>
+          <Stack.Screen name="Success" component={SuccessScreen}/>
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
